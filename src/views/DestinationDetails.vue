46963af07bbc38f066b274395609fa6f1e603242
Jiapan_Yu <2541771811@qq.com>
2020-12-25 16:31:33 +0800

    Vue Router for Everyone -- Vue Router Dynamic Routes

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,3 +1,49 @@
 <template>
-  <h2>Destination ID: {{ this.$route.params.id }}</h2>
+  <section class="destination">
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-details">
+      <img
+        :src="require(`@/assets/${destination.image}`)"
+        :alt="destination.name"
+      />
+      <p>{{ destination.description }}</p>
+    </div>
+  </section>
 </template>
+
+<script>
+import store from "@/store.js";
+
+export default {
+  data() {
+    return {
+      destinationID: this.$route.params.id
+    };
+  },
+  computed: {
+    destination: function() {
+      return store.destinations.find(
+        destination => destination.id === this.destinationID
+      );
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  max-width: 600px;
+  height: auto;
+  width: 100%;
+  max-height: 400px;
+}
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
