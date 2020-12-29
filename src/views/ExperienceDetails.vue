58c85f0b71f699ea90e2091a630872e75bcba29d
Jiapan_Yu <2541771811@qq.com>
2020-12-29 17:21:25 +0800

    Vue Router Nested Routes -- add Top experiences cards

---
+++ src/views/ExperienceDetails.vue
@@ -0,0 +1,17 @@
+<style scoped>
+img {
+  max-width: 600px;
+  height: auto;
+  width: 100%;
+  max-height: 400px;
+}
+.experience-details {
+  display: flex;
+  justify-content: space-between;
+}
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
