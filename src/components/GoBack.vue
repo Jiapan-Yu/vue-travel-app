28dcb162f2d2c32504ad323159ccde36e358c26c
Jiapan_Yu <2541771811@qq.com>
2020-12-30 11:52:46 +0800

    Creating a Go-Back button

---
+++ src/components/GoBack.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="go-back" @click="goBack">go back</span>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack: function() {
+      this.$router.go(-1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.go-back {
+  display: flex;
+  cursor: pointer;
+}
+button {
+  border: 0;
+};
+</style>
