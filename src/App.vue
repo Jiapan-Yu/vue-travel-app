5e5037e12005d9f5d588e0311eed55199c3f3b6e
Jiapan_Yu <2541771811@qq.com>
2021-01-04 15:18:31 +0800

    How to control the scroll behavior of Vue Router: 删除这个就不会点击 experience 的时候突然跳到顶部

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <TheNavigation />
-    <transition name="fade" mode="out-in">
+    <transition>
       <router-view :key="$route.path" />
     </transition>
   </div>
